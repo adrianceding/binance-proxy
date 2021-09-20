e9fd237811c583c1556e329ec31e10b06b8583dc
adriance <adrianceding@gmail.com>
2021-09-20 22:43:56 +0800

    sync

--- src/binance-proxy/go.mod
+++ src/binance-proxy/go.mod
@@ -4,9 +4,9 @@ go 1.15
 
 require (
 	github.com/adshao/go-binance/v2 v2.3.1
-	github.com/beego/bee v1.12.3 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
 )
--- src/binance-proxy/handler/handler.go
+++ src/binance-proxy/handler/handler.go
@@ -8,6 +8,7 @@ import (
 	"net/url"
 
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/time/rate"
 )
 
 func NewHandler(ctx context.Context, class service.Class) func(w http.ResponseWriter, r *http.Request) {
@@ -15,12 +16,19 @@ func NewHandler(ctx context.Context, class service.Class) func(w http.ResponseWr
 		srv:   service.NewService(ctx, class),
 		class: class,
 	}
+	if class == service.SPOT {
+		handler.limiter = rate.NewLimiter(20, 1200)
+	} else {
+		handler.limiter = rate.NewLimiter(40, 2400)
+	}
+
 	return handler.Router
 }
 
 type Handler struct {
-	class service.Class
-	srv   *service.Service
+	class   service.Class
+	srv     *service.Service
+	limiter *rate.Limiter
 }
 
 func (s *Handler) Router(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +56,12 @@ func (s *Handler) reverseProxy(w http.ResponseWriter, r *http.Request) {
 	} else {
 		r.Host = "fapi.binance.com"
 		u, _ = url.Parse("https://fapi.binance.com")
+	}
+
+	if s.class == service.SPOT {
+		s.limiter.WaitN(context.Background(), 1)
+	} else {
+		s.limiter.WaitN(context.Background(), 5)
 	}
 
 	proxy := httputil.NewSingleHostReverseProxy(u)
